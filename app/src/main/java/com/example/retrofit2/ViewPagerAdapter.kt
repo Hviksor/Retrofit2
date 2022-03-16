311197a47bbfe74389474c16ad607cec05fc23ed
Ololo <vsvktor@gmail.com>
2022-03-16 22:45:03 +0300

    Part3_15.03.22

---
+++ app/src/main/java/com/example/retrofit2/ViewPagerAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.retrofit2
+
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.retrofit2.screens.second.SecondFragment
+import com.example.retrofit2.screens.start.StartFragment
+
+class ViewPagerAdapter()  {
+
+}
